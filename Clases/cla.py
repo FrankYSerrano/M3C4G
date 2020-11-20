d6429f89cd890ffa893eb9a59bbe396f75ae26a4
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-20 19:39:41 -0300

    clases de pag 2 de pdf

---
+++ Clases/cla.py
@@ -0,0 +1,170 @@
+# Definicion de Clase
+class Guardia:
+   # Definicion de contructor de inicializacion
+    def __init__ (self, arg1, arg2, arg3):
+        self.a1 = arg1
+        self.a2 = arg2
+        self.a3 = arg3
+
+   # Definicion de metodo 1
+    def mGuardia_1(self):
+        print("Clase Guardia - Metodo 1") 
+
+   # Definicion de metodo 2
+    def mGuardia_2(self):
+        print("Clase Guardia - Metodo 2") 
+
+   # Definicion de metodo 3
+    def mGuardia_3(self):
+        print("Clase Guardia - Metodo 3") 
+
+   # Definicion de metodo 4
+    def mGuardia_4(self):
+        print("Clase Guardia - Metodo 4") 
+
+class UnidadHabitacional:
+   # Definicion de contructor de inicializacion
+    def __init__ (self, arg1, arg2, arg3):
+        self.a1 = arg1
+        self.a2 = arg2
+        self.a3 = arg3
+
+   # Definicion de metodo 1
+    def mUnidadHab_1(self):
+        print("Clase UnidadHab1 - Metodo 1") 
+
+   # Definicion de metodo 2
+    def mUnidadHab_2(self):
+        print("Clase UnidadHab1 - Metodo 2") 
+
+   # Definicion de metodo 3
+    def mUnidadHab_3(self):
+        print("Clase UnidadHab1 - Metodo 3") 
+
+   # Definicion de metodo 4
+    def mUnidadHab_4(self):
+        print("Clase UnidadHab1 - Metodo 4") 
+
+class CuentaCorriente:
+   # Definicion de contructor de inicializacion
+    def __init__ (self, arg1, arg2, arg3):
+        self.a1 = arg1
+        self.a2 = arg2
+        self.a3 = arg3
+
+   # Definicion de metodo 1
+    def mCtaCorr_1(self):
+        print("Clase CtaCorr - Metodo 1") 
+
+   # Definicion de metodo 2
+    def mCtaCorr_2(self):
+        print("Clase CtaCorr - Metodo 2") 
+
+   # Definicion de metodo 3
+    def mCtaCorr_3(self):
+        print("Clase CtaCorr - Metodo 3") 
+
+   # Definicion de metodo 4
+    def mCtaCorr_4(self):
+        print("Clase CtaCorr - Metodo 4") 
+
+class Terreno:
+   # Definicion de contructor de inicializacion
+    def __init__ (self, arg1, arg2, arg3, arg4, arg5, arg6):
+        self.a1 = arg1
+        self.a2 = arg2
+        self.a3 = arg3
+        self.a3 = arg4
+        self.a3 = arg5
+        self.a3 = arg6
+
+   # Definicion de metodo 1
+    def mTerr_1(self):
+        print("Clase Terreno - Metodo 1") 
+
+   # Definicion de metodo 2
+    def mTerr_2(self):
+        print("Clase Terreno - Metodo 2") 
+
+   # Definicion de metodo 3
+    def mTerr_3(self):
+        print("Clase Terreno - Metodo 3") 
+
+   # Definicion de metodo 4
+    def mTerr_4(self):
+        print("Clase Terreno - Metodo 4") 
+
+   # Definicion de metodo 5
+    def mTerr_5(self):
+        print("Clase Terreno - Metodo 5") 
+
+   # Definicion de metodo 6
+    def mTerr_6(self):
+        print("Clase Terreno - Metodo 6") 
+
+class CondominoVertical:
+   # Definicion de contructor de inicializacion
+    def __init__ (self, arg1, arg2, arg3, arg4, arg5):
+        self.a1 = arg1
+        self.a2 = arg2
+        self.a3 = arg3
+        self.a3 = arg4
+        self.a3 = arg5
+
+   # Definicion de metodo 1
+    def mCondVert_1(self):
+        print("Clase CondVert - Metodo 1") 
+
+   # Definicion de metodo 2
+    def mCondVert_2(self):
+        print("Clase CondVert - Metodo 2") 
+
+   # Definicion de metodo 3
+    def mCondVert_3(self):
+        print("Clase CondVert - Metodo 3") 
+
+   # Definicion de metodo 4
+    def mCondVert_4(self):
+        print("Clase CondVert - Metodo 4") 
+
+   # Definicion de metodo 5
+    def mCondVert_5(self):
+        print("Clase CondVert - Metodo 5") 
+
+   # Definicion de metodo 6
+    def mCondVert_6(self):
+        print("Clase CondVert - Metodo 6") 
+
+class CondominioHorizontal:
+   # Definicion de contructor de inicializacion
+    def __init__ (self, arg1, arg2, arg3, arg4, arg5):
+        self.a1 = arg1
+        self.a2 = arg2
+        self.a3 = arg3
+        self.a3 = arg4
+        self.a3 = arg5
+
+   # Definicion de metodo 1
+    def mCondHoriz_1(self):
+        print("Clase CondHoriz - Metodo 1") 
+
+   # Definicion de metodo 2
+    def mCondHoriz_2(self):
+        print("Clase CondHoriz - Metodo 2") 
+
+   # Definicion de metodo 3
+    def mCondHoriz_3(self):
+        print("Clase CondHoriz - Metodo 3") 
+
+   # Definicion de metodo 4
+    def mCondHoriz_4(self):
+        print("Clase CondHoriz - Metodo 5") 
+
+   # Definicion de metodo 5
+    def mCondHoriz_5(self):
+        print("Clase CondHoriz - Metodo 5") 
+
+   # Definicion de metodo 6
+    def mCondHoriz_6(self):
+        print("Clase CondHoriz - Metodo 6") 
+
